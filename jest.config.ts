ae62a66a4e2633cd951327fa3d776c74ce525ecd
Artem Lobovskii <lobovskiy@yandex.ru>
2023-08-04 02:05:10 +0300

    chore: supplement basic jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,13 @@
-export default {
+import type { Config } from 'jest';
+
+const config: Config = {
+  clearMocks: true,
+  roots: ['<rootDir>/src'],
   testEnvironment: 'jsdom',
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
 };
+
+export default config;
