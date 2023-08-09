84aa6a718b97e694a4f43a47b644ccf589da586e
OKitel <kitelolga@gmail.com>
2023-08-09 17:56:39 +0200

    feat: add router into project

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+
 export const App: React.FC = (): JSX.Element => {
   return (
     <>
-      <h1 id="title">eCommerce App</h1>
+      <Routes>
+        <Route path="/" element={<h1>Main Page</h1>}></Route>
+        <Route path="/login" element={<h1>Login Page</h1>}></Route>
+        <Route path="/registration" element={<h1>Registration Page</h1>}></Route>
+      </Routes>
     </>
   );
 };
