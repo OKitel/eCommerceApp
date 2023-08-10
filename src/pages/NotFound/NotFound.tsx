4747ce4972f01c26e775c3b7b596cbe7d8741820
OKitel <kitelolga@gmail.com>
2023-08-10 23:31:12 +0200

    feat: add link to main on 404 page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './styles.scss';
 
 export const NotFound: React.FC = (): JSX.Element => {
@@ -5,11 +6,14 @@ export const NotFound: React.FC = (): JSX.Element => {
     <>
       <div className="wrapper">
         <div className="container">
-          <h2 className="title text">4</h2>
+          <h2 className="title">4</h2>
           <img className="vinyl" src="./src/assets/vinyl.png" alt="vinyl record melts" />
-          <h2 className="title text">4</h2>
+          <h2 className="title">4</h2>
         </div>
-        <h2 className="subtitle text">Page not found</h2>
+        <h2 className="subtitle">Page not found</h2>
+        <Link className="btn" to={'/'}>
+          Back to main
+        </Link>
       </div>
     </>
   );
