cb8cfb8ead1b131d2e167c2b1788a676eda693db
OKitel <kitelolga@gmail.com>
2023-08-14 22:57:47 +0200

    feat: set custom mui theme

---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#673ab7',
+    },
+    secondary: {
+      main: '#89b73a',
+    },
+  },
+});
