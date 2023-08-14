63c2f1da973c739b0372a6cf0898a48672beb4c0
Artem Lobovskii <lobovskiy@yandex.ru>
2023-08-14 16:36:01 +0300

    feat: add api tests

---
+++ src/__tests__/api.test.tsx
@@ -0,0 +1,52 @@
+/**
+ * @jest-environment node
+ */
+import serviceApi from '../api/Service';
+import anonymousApi from '../api/Anonymous';
+import spaApi from '../api/Spa';
+
+const CREATED_TEST_CUSTOMER_EMAIL = process.env.VITE_CTP_CREATED_TEST_CUSTOMER_EMAIL || '';
+const CREATED_TEST_CUSTOMER_PASSWORD = process.env.VITE_CTP_CREATED_TEST_CUSTOMER_PASSWORD || '';
+const TEMP_CUSTOMER_EMAIL = 'temp@example.com';
+const TEMP_CUSTOMER_PASSWORD = 'temp1234';
+
+describe('service api', () => {
+  it('creates and deletes a customer', async () => {
+    const responseCreateCustomer = await serviceApi.createCustomer({
+      email: TEMP_CUSTOMER_EMAIL,
+      password: TEMP_CUSTOMER_PASSWORD,
+    });
+    const {
+      body: {
+        customer: { id, version },
+      },
+    } = responseCreateCustomer;
+
+    expect(id).toBeDefined();
+    expect(version).toBeDefined();
+
+    const responseDeleteCustomer = await serviceApi.deleteCustomer(id, version);
+
+    expect(responseDeleteCustomer.body.id).toEqual(id);
+  });
+});
+
+describe('anonymous api', () => {
+  it('logs a customer in', async () => {
+    const responseLoginCustomer = await anonymousApi.login(CREATED_TEST_CUSTOMER_EMAIL, CREATED_TEST_CUSTOMER_PASSWORD);
+
+    expect(responseLoginCustomer.body.customer.email).toEqual(CREATED_TEST_CUSTOMER_EMAIL);
+  });
+});
+
+describe('SPA api', () => {
+  it('log a customer in', async () => {
+    spaApi.obtainCustomerToken(CREATED_TEST_CUSTOMER_EMAIL, CREATED_TEST_CUSTOMER_PASSWORD);
+    const responseLoginCustomer = await spaApi.loginCustomer(
+      CREATED_TEST_CUSTOMER_EMAIL,
+      CREATED_TEST_CUSTOMER_PASSWORD,
+    );
+
+    expect(responseLoginCustomer?.body.customer.email).toEqual(CREATED_TEST_CUSTOMER_EMAIL);
+  });
+});
