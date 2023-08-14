7890e02c50439f033b7803e6a9a973ded1ea9b80
Artem Lobovskii <lobovskiy@yandex.ru>
2023-08-14 15:23:29 +0300

    feat: add delete customer api method

--- src/api/Service.ts
+++ src/api/Service.ts
@@ -1,11 +1,15 @@
 import { serviceApiRoot } from '../lib/commercetools-sdk';
 import { ClientResponse } from '@commercetools/platform-sdk/dist/declarations/src/generated/shared/utils/common-types';
-import { CustomerSignInResult } from '@commercetools/platform-sdk/dist/declarations/src/generated/';
+import { CustomerSignInResult, Customer } from '@commercetools/platform-sdk/dist/declarations/src/generated/';
 import { CustomerDraft } from '@commercetools/platform-sdk/dist/declarations/src/generated/';
 
 class ServiceApi {
   public createCustomer(customerData: CustomerDraft): Promise<ClientResponse<CustomerSignInResult>> {
     return serviceApiRoot.customers().post({ body: customerData }).execute();
+  }
+
+  public deleteCustomer(ID: string, version: number): Promise<ClientResponse<Customer>> {
+    return serviceApiRoot.customers().withId({ ID }).delete({ queryArgs: { version } }).execute();
   }
 }
 
