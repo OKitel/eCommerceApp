ddf576562a2097ed826204e0a906966f8c1ea6f1
OKitel <kitelolga@gmail.com>
2023-08-19 00:32:11 +0200

    feat: add checkbox input

---
+++ src/components/form-components/FormCheckBox.tsx
@@ -0,0 +1,26 @@
+import { Controller, UseControllerProps } from 'react-hook-form';
+import { Checkbox, FormLabel } from '@mui/material';
+
+interface FormInputProps extends UseControllerProps {
+  label: string;
+}
+
+export const FormCheckBox = ({ name, control, rules, label }: FormInputProps): JSX.Element => {
+  return (
+    <Controller
+      name={name}
+      control={control}
+      rules={rules}
+      render={({ field }): JSX.Element => (
+        <>
+          <Checkbox
+            {...field}
+            checked={field.value ?? false}
+            onChange={(e): void => field.onChange(e.target.checked)}
+          />
+          <FormLabel>{label}</FormLabel>
+        </>
+      )}
+    />
+  );
+};
