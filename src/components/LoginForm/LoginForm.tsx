e38e40bfb37a1c123e2cab13405dc7abd8cc0c04
Artem Lobovskii <lobovskiy@yandex.ru>
2023-08-19 04:33:21 +0300

    fix: remove validation for login password input

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -5,7 +5,7 @@ import { FieldValues, useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import { loginCustomer } from '../../slices/customerSlice';
 import { useAppDispatch } from '../../store/hooks';
-import { EMAIL_REGEXP, PASSWORD_REGEXP } from '../../consts';
+import { EMAIL_REGEXP } from '../../consts';
 import './styles.scss';
 
 // eslint-disable-next-line max-lines-per-function
@@ -35,20 +35,7 @@ export const LoginForm: React.FC = (): JSX.Element => {
               pattern: { value: EMAIL_REGEXP, message: 'Please enter a valid email address' },
             }}
           />
-          <FormInputText
-            name="password"
-            control={control}
-            label="Password"
-            type="password"
-            rules={{
-              required: 'Password is required',
-              pattern: {
-                value: PASSWORD_REGEXP,
-                message:
-                  'Minimum 8 characters, at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character',
-              },
-            }}
-          />
+          <FormInputText name="password" control={control} label="Password" type="password" />
           <div className="form-btn">
             <Button className="form-btn" variant="contained" type="submit">
               Login
