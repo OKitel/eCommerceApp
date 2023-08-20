bfaf231699a14c2c836ede7dd4e05b38d4a1be08
Artem Lobovskii <lobovskiy@yandex.ru>
2023-08-21 01:39:36 +0300

    feat: add getting logged in customer info and saving it in store

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,19 @@
+import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { NotFound } from '../pages/NotFound/NotFound';
 import { Header } from './Header/Header';
 import { Registration } from '../pages/Registration/Registration';
 import { Login } from '../pages/Login/Login';
+import { useAppDispatch } from '../store/hooks';
+import { getLoggedInCustomer } from '../slices/customerSlice';
 
 export const App: React.FC = (): JSX.Element => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getLoggedInCustomer());
+  }, [dispatch]);
+
   return (
     <>
       <Header />
