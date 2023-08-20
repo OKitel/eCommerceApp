c74ae26f2942b4bedc8cf756af6ba0de8be2e81e
OKitel <kitelolga@gmail.com>
2023-08-21 01:00:34 +0200

    feat: add styles for main page

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,12 +1,20 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+import maestroUrl from '../../assets/images/maestro.png';
 import './styles.scss';
 
 export const Main: React.FC = (): JSX.Element => {
   return (
     <>
-      <Box display="flex" justifyContent="center" alignItems="center">
-        <h2>Welcome to Maestro!</h2>
-        <p>Oline market, where melodies come alive</p>
+      <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column">
+        <h2 className="welcome-title">Welcome to Maestro!</h2>
+        <Typography variant="subtitle1">Oline market, where melodies come alive</Typography>
+        <div className="maestro-container">
+          <img className="maestro" src={maestroUrl} alt="maestro on treble clef" />
+        </div>
+
+        <Typography className="welcome-subtitle" variant="subtitle2">
+          Find your perfect instrument in a world of musical possibilities
+        </Typography>
       </Box>
     </>
   );
