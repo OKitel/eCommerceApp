9cb95a31288bd7f55e4211ee00d76460e0829a5f
Artem Lobovskii <lobovskiy@yandex.ru>
2023-08-21 01:47:59 +0300

    feat: add alerts slice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import customerReducer from '../slices/customerSlice';
+import alertsReducer from '../slices/alertsSlice';
 
 export const store = configureStore({
   reducer: {
     customer: customerReducer,
+    alerts: alertsReducer,
   },
 });
 
