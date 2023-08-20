4493f79484d363c481cb688a0dd655a9c0574422
OKitel <kitelolga@gmail.com>
2023-08-21 01:42:23 +0200

    feat: add image on cart page

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,4 +1,5 @@
 import { Box } from '@mui/material';
+import wipUrl from '../../assets/images/wip.png';
 import './styles.scss';
 
 export const Cart: React.FC = (): JSX.Element => {
@@ -6,6 +7,10 @@ export const Cart: React.FC = (): JSX.Element => {
     <>
       <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column">
         <h2 className="cart-title">Cart Page</h2>
+        <div className="wip-container">
+          <img className="wip-image" src={wipUrl} alt="work in progress" />
+          <h4 className="wip-text">WORK IN PROGRESS</h4>
+        </div>
       </Box>
     </>
   );
