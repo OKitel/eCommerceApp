786ed971e09f801cd99b77e3a2e0d1026af2929b
Artem Lobovskii <lobovskiy@yandex.ru>
2023-08-20 17:07:55 +0300

    feat: add alerts to login actions

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Paper, Box } from '@mui/material';
+import React, { useState } from 'react';
+import { Paper, Box, Snackbar, Alert, SlideProps, Slide } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
 import { FormInputText } from '../form-components/FormInputText';
 import { FieldValues, useForm } from 'react-hook-form';
@@ -9,55 +9,111 @@ import { useAppSelector, useAppDispatch } from '../../store/hooks';
 import { EMAIL_REGEXP, PASSWORD_REGEXP } from '../../consts';
 import './styles.scss';
 
+type TransitionProps = Omit<SlideProps, 'direction'>;
+
+const TransitionDown = (props: TransitionProps): JSX.Element => {
+  return <Slide {...props} direction="down" />;
+};
+
 // eslint-disable-next-line max-lines-per-function
 export const LoginForm: React.FC = (): JSX.Element => {
   const { control, handleSubmit } = useForm();
   const progressLogin = useAppSelector((state) => state.customer.progress.login);
+  const errorMessage = useAppSelector((state) => state.customer.errorMessage);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
-  const onSubmit = ({ email, password }: FieldValues): void => {
-    const onSuccess = (): void => navigate('/');
+  const [isLoginError, setIsLoginError] = useState(false);
+  const [isLoginSuccess, setIsLoginSuccess] = useState(false);
 
-    dispatch(loginCustomer({ email, password, onSuccess }));
+  const handleCloseSnackbar = (_?: React.SyntheticEvent | Event, reason?: string): void => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setIsLoginError(false);
+    setIsLoginSuccess(false);
+  };
+
+  const onSubmit = ({ email, password }: FieldValues): void => {
+    const onSuccess = (): void => {
+      setIsLoginError(false);
+      setIsLoginSuccess(true);
+      setTimeout(() => {
+        navigate('/');
+      }, 3000);
+    };
+    const onError = (): void => {
+      setIsLoginError(true);
+      setIsLoginSuccess(false);
+    };
+
+    dispatch(loginCustomer({ email, password, onSuccess, onError }));
   };
 
   return (
-    <Box sx={{ width: 'clamp(28rem, calc(100% - 4rem), 40rem)', margin: '10rem auto' }}>
-      <Paper elevation={3} sx={{ padding: '2rem' }}>
-        <h2 className="form-title">Login Form</h2>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <FormInputText
-            name="email"
-            control={control}
-            label="Email"
-            type="email"
-            rules={{
-              required: 'Email is required',
-              pattern: { value: EMAIL_REGEXP, message: 'Please enter a valid email address' },
-            }}
-          />
-          <FormInputText
-            name="password"
-            control={control}
-            label="Password"
-            type="password"
-            rules={{
-              required: 'Password is required',
-              pattern: {
-                value: PASSWORD_REGEXP,
-                message:
-                  'Minimum 8 characters, at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character',
-              },
-            }}
-          />
-          <div className="form-btn">
-            <LoadingButton loading={progressLogin} className="form-btn" variant="contained" type="submit">
-              Login
-            </LoadingButton>
-          </div>
-        </form>
-      </Paper>
-    </Box>
+    <>
+      <Snackbar
+        sx={{ whiteSpace: 'pre-line' }}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        open={isLoginError}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+        TransitionComponent={TransitionDown}
+      >
+        <Alert severity="error" onClose={(): void => setIsLoginError(false)}>
+          {`Oops! Login failed.\n${errorMessage}`}
+        </Alert>
+      </Snackbar>
+
+      <Snackbar
+        sx={{ whiteSpace: 'pre-line' }}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        open={isLoginSuccess}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+        TransitionComponent={TransitionDown}
+      >
+        <Alert severity="success" onClose={(): void => setIsLoginSuccess(false)}>
+          {'You have successfully logged in!\nWe are now redirecting you to the main page...'}
+        </Alert>
+      </Snackbar>
+      <Box sx={{ width: 'clamp(28rem, calc(100% - 4rem), 40rem)', margin: '10rem auto' }}>
+        <Paper elevation={3} sx={{ padding: '2rem' }}>
+          <h2 className="form-title">Login Form</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <FormInputText
+              name="email"
+              control={control}
+              label="Email"
+              type="email"
+              rules={{
+                required: 'Email is required',
+                pattern: { value: EMAIL_REGEXP, message: 'Please enter a valid email address' },
+              }}
+            />
+            <FormInputText
+              name="password"
+              control={control}
+              label="Password"
+              type="password"
+              rules={{
+                required: 'Password is required',
+                pattern: {
+                  value: PASSWORD_REGEXP,
+                  message:
+                    'Minimum 8 characters, at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character',
+                },
+              }}
+            />
+            <div className="form-btn">
+              <LoadingButton loading={progressLogin} className="form-btn" variant="contained" type="submit">
+                Login
+              </LoadingButton>
+            </div>
+          </form>
+        </Paper>
+      </Box>
+    </>
   );
 };
