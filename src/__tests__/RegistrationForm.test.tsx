96bd842f9284fa42d25c56175abde883b727d5eb
OKitel <kitelolga@gmail.com>
2023-08-20 16:18:18 +0200

    feat: add test for email with valid email

--- src/__tests__/RegistrationForm.test.tsx
+++ src/__tests__/RegistrationForm.test.tsx
@@ -3,18 +3,19 @@ import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import { renderWithProviders } from './test-utils';
 import { RegistrationForm } from '../components/RegistrationForm/RegistrationForm';
-import { debug } from 'jest-preview';
 
 // eslint-disable-next-line max-lines-per-function
 describe('Registration form validation', () => {
-  test('Registration form required validation', async () => {
-    console.log('test 1');
-    const user = userEvent.setup();
+  const user = userEvent.setup();
+  const renderComponent = (): void => {
     renderWithProviders(
       <MemoryRouter initialEntries={['/registration']}>
         <RegistrationForm />
       </MemoryRouter>,
     );
+  };
+  test('Registration form required validation', async () => {
+    renderComponent();
     await user.click(screen.getByTestId('submit-btn'));
     expect(screen.getAllByText('Name is required')[0]).toBeInTheDocument();
     expect(screen.getAllByText('Last name is required')[0]).toBeInTheDocument();
@@ -28,18 +29,18 @@ describe('Registration form validation', () => {
   });
 
   test('Registration form invalid email validation', async () => {
-    console.log('test 2');
-    const user = userEvent.setup();
-    renderWithProviders(
-      <MemoryRouter initialEntries={['/registration']}>
-        <RegistrationForm />
-      </MemoryRouter>,
-    );
-
+    renderComponent();
     const emailInput = screen.getByTestId('email').querySelector('input');
     if (emailInput) await user.type(emailInput, 'invalid-email');
     await user.click(screen.getByTestId('submit-btn'));
-    debug();
     expect(screen.getByText('Please enter a valid email address')).toBeInTheDocument();
+  });
+
+  test('Registration form valid email validation', async () => {
+    renderComponent();
+    const emailInput = screen.getByTestId('email').querySelector('input');
+    if (emailInput) await user.type(emailInput, 'test@gmail.com');
+    await user.click(screen.getByTestId('submit-btn'));
+    expect(screen.queryByText('Please enter a valid email address')).not.toBeInTheDocument();
   });
 });
