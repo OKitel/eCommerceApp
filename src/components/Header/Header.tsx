92ffe4815e6e0eb77382896487f4846db8b13103
LeszekSokolov <Leszek.Sokolov@gmail.com>
2023-08-21 16:08:46 +0300

    feat: add burger menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,10 @@
 import * as React from 'react';
+import { useState } from 'react';
+import Hidden from '@mui/material/Hidden';
+import MenuIcon from '@mui/icons-material/Menu';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import Divider from '@mui/material/Divider';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -17,6 +23,7 @@ export const Header: React.FC = (): JSX.Element => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const handleClickCart = (): void => navigate('/cart');
+  const [open, setOpen] = useState(false);
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -28,34 +35,114 @@ export const Header: React.FC = (): JSX.Element => {
               <span className="logo-sub">Market</span>
             </RouterLink>
           </div>
-          <div>
-            <IconButton size="medium" color="inherit" onClick={handleClickCart}>
-              <ShoppingCartRoundedIcon />
+          <Hidden smDown>
+            <div>
+              <IconButton size="medium" color="inherit" onClick={handleClickCart}>
+                <ShoppingCartRoundedIcon />
+              </IconButton>
+              {progressIntrospect ? null : customerData ? (
+                <Button
+                  component={RouterLink}
+                  to="/"
+                  variant="contained"
+                  color="secondary"
+                  onClick={(): void => {
+                    dispatch(clearCustomerData());
+                  }}
+                >
+                  Logout
+                </Button>
+              ) : (
+                <>
+                  <Button component={RouterLink} to="/login" variant="contained" sx={{ m: 1 }} color="secondary">
+                    Login
+                  </Button>
+                  <Button component={RouterLink} to="/registration" color="secondary" variant="contained">
+                    Register
+                  </Button>
+                </>
+              )}
+            </div>
+          </Hidden>
+          <Hidden smUp>
+            <IconButton onClick={(): void => setOpen(true)} color="secondary">
+              <MenuIcon />
             </IconButton>
-            {progressIntrospect ? null : customerData ? (
+          </Hidden>
+        </Toolbar>
+        <SwipeableDrawer
+          anchor="right"
+          open={open}
+          onOpen={(): void => setOpen(true)}
+          onClose={(): void => setOpen(false)}
+        >
+          <div onClick={(): void => setOpen(false)} onKeyUp={(): void => setOpen(false)} role="button" tabIndex={0}>
+            <IconButton>
+              <ChevronRightIcon />
+            </IconButton>
+          </div>
+          <Divider />
+          <IconButton
+            size="medium"
+            color="inherit"
+            onClick={(): void => {
+              handleClickCart();
+              setOpen(false);
+            }}
+          >
+            <ShoppingCartRoundedIcon />
+          </IconButton>
+          {progressIntrospect ? null : customerData ? (
+            <Button
+              component={RouterLink}
+              to="/"
+              variant="contained"
+              color="secondary"
+              onClick={(): void => {
+                dispatch(clearCustomerData());
+                setOpen(false);
+              }}
+              sx={{
+                width: 180,
+                ml: 1,
+                mr: 1,
+              }}
+            >
+              Logout
+            </Button>
+          ) : (
+            <>
               <Button
                 component={RouterLink}
-                to="/"
+                to="/login"
                 variant="contained"
+                sx={{ m: 1, width: 180, ml: 1, mr: 1 }}
                 color="secondary"
                 onClick={(): void => {
-                  dispatch(clearCustomerData());
+                  setOpen(false);
                 }}
               >
-                Logout
+                Login
               </Button>
-            ) : (
-              <>
-                <Button component={RouterLink} to="/login" variant="contained" sx={{ m: 1 }} color="secondary">
-                  Login
-                </Button>
-                <Button component={RouterLink} to="/registration" color="secondary" variant="contained">
-                  Register
-                </Button>
-              </>
-            )}
-          </div>
-        </Toolbar>
+              <Button
+                component={RouterLink}
+                to="/registration"
+                color="secondary"
+                variant="contained"
+                sx={{
+                  width: 180,
+                  ml: 1,
+                  mr: 1,
+                }}
+                onClick={(): void => {
+                  setOpen(false);
+                }}
+              >
+                Register
+              </Button>
+            </>
+          )}
+        </SwipeableDrawer>
       </AppBar>
     </Box>
   );
