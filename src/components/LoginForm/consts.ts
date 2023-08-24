7592648e571f1d6ba762c99153ab93309f73218a
Artem Lobovskii <lobovskiy@yandex.ru>
2023-08-25 01:18:55 +0300

    refactor: move text values to consts

---
+++ src/components/LoginForm/consts.ts
@@ -0,0 +1,23 @@
+export const TITLES = {
+  form: 'Login Form',
+};
+
+export const LABELS = {
+  fieldEmail: 'Email',
+  fieldPassword: 'Password',
+  btnSubmitForm: 'Login',
+};
+
+export const TEXT_CONTENT = {
+  fieldEmail: {
+    required: 'Email is required',
+    patternMessage: 'Please enter a valid email address',
+  },
+  fieldPassword: {
+    required: 'Password is required',
+    patternMessage:
+      'Minimum 8 characters, at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character',
+  },
+  messageSuccesLogin: 'You have successfully logged in!',
+  messageErrorLogin: `Oops! Login failed.`,
+};
