62f0acfcc9984db93777514b835cbc9ae5f2db61
Artem Lobovskii <lobovskiy@yandex.ru>
2023-08-26 16:59:41 +0300

    feat: add modules for common style consts

---
+++ src/theme/consts.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  primary: '#673ab7',
+  primaryDark: '#311b92',
+  secondary: '#c6a7fd',
+  secondaryContrast: '#fff',
+  textPrimary: '#453e53',
+};
+
+export const fontSizes = {
+  400: '1rem',
+  800: '2.5rem',
+};
