0153c8678d44f16bef43b171d516112c6ca85296
OKitel <kitelolga@gmail.com>
2023-08-26 11:30:55 +0200

    feat: add routes to profile and catalog pages

---
+++ src/pages/Catalog/Catalog.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@mui/material';
+
+export const Catalog: React.FC = (): JSX.Element => {
+  return (
+    <>
+      <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column">
+        <h2 className="cart-title">Catalog Page</h2>
+      </Box>
+    </>
+  );
+};
