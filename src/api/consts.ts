e6e95b355b978358b0ec22a0c919e8fa22e252ae
Artem Lobovskii <lobovskiy@yandex.ru>
2023-08-27 15:02:30 +0300

    fix: fix client error response check

---
+++ src/api/consts.ts
@@ -0,0 +1,3 @@
+export const API_ERROR_CODES = {
+  invalidGrant: 'invalid_grant',
+};
--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,9 +1,22 @@
-import { ErrorResponse } from '@commercetools/platform-sdk/dist/declarations/src/generated/';
+import {
+  ClientResponse,
+  ErrorResponse,
+  AuthErrorResponse,
+} from '@commercetools/platform-sdk/dist/declarations/src/generated/';
 import { TokenStoreTypes } from '../lib/commercetools-sdk';
 import { clearTokenStore } from '../utils/localStorage';
 import { ValidationErrorResponse } from './types';
+import { API_ERROR_CODES } from './consts';
 
 const MAX_RETRYING_ATTEMPTS_NUMBER = 5;
+
+export function isClientResponse(object: unknown): object is ClientResponse {
+  if (typeof object === 'object' && object !== null && object.hasOwnProperty('body')) {
+    return true;
+  }
+
+  return false;
+}
 
 export function isErrorResponse(object: unknown): object is ErrorResponse {
   if (
@@ -13,6 +26,25 @@ export function isErrorResponse(object: unknown): object is ErrorResponse {
     object.hasOwnProperty('message')
   ) {
     return true;
+  }
+
+  return false;
+}
+
+export function isClientAuthErrorResponse(object: unknown): object is ClientResponse<AuthErrorResponse> {
+  if (isClientResponse(object)) {
+    const { body } = object;
+
+    if (
+      typeof body === 'object' &&
+      body !== null &&
+      body.hasOwnProperty('statusCode') &&
+      body.hasOwnProperty('message') &&
+      body.hasOwnProperty('error') &&
+      body.hasOwnProperty('errors')
+    ) {
+      return true;
+    }
   }
 
   return false;
@@ -34,6 +66,13 @@ export function isValidationErrorResponse(object: unknown): object is Validation
 
 export async function retry<T>(callback: () => Promise<T>, tokenStoreType: TokenStoreTypes): Promise<T> {
   let attempts = 0;
+  const isAuthErrorCode = (error: AuthErrorResponse): boolean => {
+    if (error.statusCode === 401 || (error.statusCode === 400 && error.error === API_ERROR_CODES.invalidGrant)) {
+      return true;
+    }
+
+    return false;
+  };
 
   async function executeCallback(): Promise<T> {
     try {
@@ -41,7 +80,7 @@ export async function retry<T>(callback: () => Promise<T>, tokenStoreType: Token
 
       return res;
     } catch (error) {
-      if (isErrorResponse(error) && error.statusCode === 401 && attempts < MAX_RETRYING_ATTEMPTS_NUMBER) {
+      if (isClientAuthErrorResponse(error) && isAuthErrorCode(error.body) && attempts < MAX_RETRYING_ATTEMPTS_NUMBER) {
         clearTokenStore(tokenStoreType);
 
         attempts += 1;
