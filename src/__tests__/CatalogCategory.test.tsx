227460062199bf27658c46fc67f0e42468060cd7
Artem Lobovskii <lobovskiy@yandex.ru>
2023-08-30 02:50:27 +0300

    feat: add tests

---
+++ src/__tests__/CatalogCategory.test.tsx
@@ -0,0 +1,23 @@
+import { screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { Category } from '@commercetools/platform-sdk';
+import { renderWithProviders } from './test-utils';
+import { CatalogCategory } from '../components/CatalogCategory/CatalogCategory';
+import { categories } from '../__mocks__/categories';
+
+describe('Catalog category is displayed correctly', () => {
+  const renderComponent = (category: Category): void => {
+    renderWithProviders(
+      <MemoryRouter initialEntries={['/catalog']}>
+        <CatalogCategory category={category} />
+      </MemoryRouter>,
+    );
+  };
+
+  test('Render Catalog category correctly', () => {
+    const category = categories[0];
+    renderComponent(category);
+
+    expect(screen.getByText(category.name.en)).toBeInTheDocument();
+  });
+});
