62ecdcc3b51033d632bb25c4e44fe5db200091cc
Artem Lobovskii <lobovskiy@yandex.ru>
2023-08-31 01:40:00 +0300

    feat: add rendering categories on catalog page

--- src/pages/Catalog/Catalog.tsx
+++ src/pages/Catalog/Catalog.tsx
@@ -1,14 +1,45 @@
+import { useEffect } from 'react';
 import { Box, Container, Typography } from '@mui/material';
+import classNames from 'classnames';
+
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import { getCategories } from '../../slices/categories/slice';
+import { CatalogCategory } from '../../components/CatalogCategory/CatalogCategory';
+import { ProgressLoader } from '../../components/ProgressLoader/ProgressLoader';
 
 import './styles.scss';
 
 export const Catalog: React.FC = (): JSX.Element => {
+  const dispatch = useAppDispatch();
+  const { categories, progress } = useAppSelector((state) => state.categories);
+  const contentClassName = classNames({ catalog: categories });
+
+  useEffect(() => {
+    if (!categories) {
+      dispatch(getCategories());
+    }
+  }, [categories, dispatch]);
+
+  const renderRootCategories = (): React.ReactElement | React.ReactElement[] | null => {
+    if (progress) {
+      return <ProgressLoader />;
+    }
+
+    if (!categories) {
+      return null;
+    }
+
+    const rootCategories = categories.filter((category) => !category.parent);
+
+    return rootCategories.map((category) => <CatalogCategory key={category.id} category={category} />);
+  };
+
   return (
     <Container>
       <Typography variant="h1" gutterBottom>
         Catalog
       </Typography>
-      <Box className="catalog"></Box>
+      <Box className={contentClassName}>{renderRootCategories()}</Box>
     </Container>
   );
 };
