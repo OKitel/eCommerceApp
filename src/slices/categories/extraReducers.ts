cc1f0d55646cc74d69e4762d9e74a11201b6a492
Artem Lobovskii <lobovskiy@yandex.ru>
2023-08-31 01:33:01 +0300

    feat: add categories slice

---
+++ src/slices/categories/extraReducers.ts
@@ -0,0 +1,29 @@
+import { Draft, PayloadAction } from '@reduxjs/toolkit';
+import { TCategoriesSliceState } from './types';
+import { Category } from '@commercetools/platform-sdk';
+
+export function reducerGetCategoriesPending(state: Draft<TCategoriesSliceState>): void {
+  state.progress = true;
+}
+export function reducerGetCategoriesFulfilled(
+  state: Draft<TCategoriesSliceState>,
+  action: PayloadAction<Category[] | undefined>,
+): void {
+  state.progress = false;
+  state.errorMessage = null;
+
+  if (action.payload) {
+    state.categories = action.payload;
+  }
+}
+export function reducerGetCategoriesRejected(
+  state: Draft<TCategoriesSliceState>,
+  action: PayloadAction<unknown>,
+): void {
+  const { payload } = action;
+
+  state.progress = false;
+  if (payload && typeof payload === 'string') {
+    state.errorMessage = payload;
+  }
+}
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import customerReducer from '../slices/customer/slice';
-import alertsReducer from '../slices/alerts/slice';
 import settingsReducer from '../slices/settings/slice';
+import customerReducer from '../slices/customer/slice';
+import categoriesReducer from '../slices/categories/slice';
+import alertsReducer from '../slices/alerts/slice';
 
 export const store = configureStore({
   reducer: {
     settings: settingsReducer,
     customer: customerReducer,
+    categories: categoriesReducer,
     alerts: alertsReducer,
   },
 });
