dd37b4b26eaea0b8b88cbe633cde20170c397c4e
OKitel <kitelolga@gmail.com>
2023-08-31 14:44:47 +0200

    feat: add tests for AccordionItem component

--- src/components/AddressesAccordion/AccordionItem.tsx
+++ src/components/AddressesAccordion/AccordionItem.tsx
@@ -29,17 +29,24 @@ export const AccordionItem: React.FC<Props> = ({
       <div className="address-title">
         <Typography>{address.streetName}</Typography>
         {defaultId === address.id && (
-          <Chip icon={<HomeRoundedIcon />} label="Default" color="primary" size="small" variant="outlined" />
+          <Chip
+            icon={<HomeRoundedIcon />}
+            label="Default"
+            color="primary"
+            size="small"
+            variant="outlined"
+            data-testid="chip-default-address"
+          />
         )}
       </div>
       <div className="address-controls">
         <Tooltip title="Set as default" placement="top">
-          <Switch checked={defaultId === address.id} onChange={handleChange} />
+          <Switch checked={defaultId === address.id} onChange={handleChange} data-testid="default-switch" />
         </Tooltip>
-        <IconButton onClick={onEditRequest} color="primary" className="edit-control">
+        <IconButton onClick={onEditRequest} color="primary" className="edit-control" data-testid="edit-btn">
           <EditRoundedIcon />
         </IconButton>
-        <IconButton onClick={onDeleteRequested} className="delete-control" color="error">
+        <IconButton onClick={onDeleteRequested} className="delete-control" color="error" data-testid="delete-btn">
           <DeleteRoundedIcon />
         </IconButton>
       </div>
