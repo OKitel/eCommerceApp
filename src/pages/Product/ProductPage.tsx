adec3f61ea19ee34307f43ce6e28d2525ae03674
OKitel <kitelolga@gmail.com>
2023-09-03 14:31:36 +0200

    fix: send request for new product

--- src/pages/Product/ProductPage.tsx
+++ src/pages/Product/ProductPage.tsx
@@ -1,4 +1,3 @@
-import { Product } from '@commercetools/platform-sdk';
 import { Box, Container, Typography } from '@mui/material';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import { useEffect } from 'react';
@@ -6,19 +5,28 @@ import { useParams } from 'react-router-dom';
 import { URL_PARAMS } from '../../components/consts';
 import { getProduct } from '../../slices/product/slice';
 import { ChipBreadcrumbs } from '../../components/Breadcrumbs/Breadcrumbs';
+import { ProgressLoader } from '../../components/ProgressLoader/ProgressLoader';
 
 export const ProductPage: React.FC = (): React.ReactElement => {
   const { [URL_PARAMS.productId]: productId } = useParams();
   const dispatch = useAppDispatch();
-  const maybeProduct: Product | null = useAppSelector((state) => state.product.product);
-
+  const { product: maybeProduct, progress: progressProduct } = useAppSelector((state) => state.product);
+  const { localization } = useAppSelector((state) => state.settings);
   useEffect(() => {
-    if (productId && !maybeProduct) {
+    if (productId && productId !== maybeProduct?.id) {
       dispatch(getProduct(productId));
     }
   }, [dispatch, maybeProduct, productId]);
 
-  if (!productId) {
+  if (progressProduct) {
+    return (
+      <Container>
+        <ProgressLoader />
+      </Container>
+    );
+  }
+
+  if (!productId || maybeProduct === null) {
     return (
       <Container>
         <Box marginY={3}>
@@ -36,6 +44,12 @@ export const ProductPage: React.FC = (): React.ReactElement => {
       <Container>
         <Typography variant="h1" gutterBottom>
           Product
+        </Typography>
+        <Box marginY={3}>
+          <ChipBreadcrumbs />
+        </Box>
+        <Typography variant="h3" gutterBottom>
+          {maybeProduct.masterData.current.name[localization]}
         </Typography>
       </Container>
     </>
