7c4316704f73b8114b807840e4434ea41740cbb8
OKitel <kitelolga@gmail.com>
2023-09-04 01:39:10 +0200

    feat: add product page

--- src/pages/Product/ProductPage.tsx
+++ src/pages/Product/ProductPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Container, Typography, Paper } from '@mui/material';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
@@ -6,12 +6,17 @@ import { URL_PARAMS } from '../../components/consts';
 import { getProduct } from '../../slices/product/slice';
 import { ChipBreadcrumbs } from '../../components/Breadcrumbs/Breadcrumbs';
 import { ProgressLoader } from '../../components/ProgressLoader/ProgressLoader';
+import { SwiperComponent } from '../../components/Swiper/Swiper';
+import { ProductDetails } from '../../components/ProductDetails/ProductDetails';
+import './styles.scss';
 
 export const ProductPage: React.FC = (): React.ReactElement => {
   const { [URL_PARAMS.productId]: productId } = useParams();
   const dispatch = useAppDispatch();
+
   const { product: maybeProduct, progress: progressProduct } = useAppSelector((state) => state.product);
   const { localization } = useAppSelector((state) => state.settings);
+
   useEffect(() => {
     if (productId && productId !== maybeProduct?.id) {
       dispatch(getProduct(productId));
@@ -38,19 +43,43 @@ export const ProductPage: React.FC = (): React.ReactElement => {
       </Container>
     );
   }
+  const currentProduct = maybeProduct.masterData.current;
+  const images = [];
+
+  images.push(...(currentProduct.masterVariant.images || []));
+  currentProduct.variants.forEach((variant) => {
+    images.push(...(variant.images || []));
+  });
 
   return (
     <>
       <Container>
-        <Typography variant="h1" gutterBottom>
-          Product
-        </Typography>
         <Box marginY={3}>
           <ChipBreadcrumbs />
         </Box>
+
         <Typography variant="h3" gutterBottom>
-          {maybeProduct.masterData.current.name[localization]}
+          {currentProduct.name[localization]}
         </Typography>
+
+        <Box className="product-container">
+          <SwiperComponent images={images} />
+
+          <Box className="details-container">
+            <ProductDetails product={currentProduct} />
+          </Box>
+        </Box>
+        {currentProduct.description && (
+          <Paper elevation={3} className="description-container">
+            <Typography variant="h5" className="description-title">
+              Description
+            </Typography>
+            <Typography
+              variant="body1"
+              dangerouslySetInnerHTML={{ __html: currentProduct.description[localization] }}
+            ></Typography>
+          </Paper>
+        )}
       </Container>
     </>
   );
