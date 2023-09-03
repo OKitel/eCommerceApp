b068bc31e2a57ac228796242f8e451382b5214a6
OKitel <kitelolga@gmail.com>
2023-09-04 00:53:20 +0200

    feat: add swiper for product images

---
+++ src/components/Swiper/Swiper.tsx
@@ -0,0 +1,64 @@
+import { Swiper as SwiperReact, SwiperSlide } from 'swiper/react';
+import { Navigation, Thumbs, FreeMode } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import { Paper, Typography } from '@mui/material';
+import { useState } from 'react';
+import { Swiper } from 'swiper/types';
+import './styles.scss';
+
+type Props = {
+  images: { url: string }[] | undefined;
+};
+
+export const SwiperComponent: React.FC<Props> = ({ images }: Props): React.ReactElement => {
+  const [thumbsSwiper, setThumbsSwiper] = useState<Swiper | null>(null);
+  return (
+    <Paper className="swiper-container" elevation={3}>
+      <SwiperReact
+        loop
+        thumbs={{ swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null }}
+        modules={[Navigation, Thumbs, FreeMode]}
+        spaceBetween={50}
+        navigation
+        className="main-swiper"
+      >
+        {!images && <Typography variant="h3">No image found</Typography>}
+        {images &&
+          images.map((image) => {
+            return (
+              <SwiperSlide key={image.url}>
+                <img
+                  id="main-swiper_image"
+                  src={image.url}
+                  alt="product image"
+                  onClick={(): void => console.log('open modal with image')}
+                />
+              </SwiperSlide>
+            );
+          })}
+      </SwiperReact>
+      <SwiperReact
+        onSwiper={setThumbsSwiper}
+        loop={true}
+        spaceBetween={10}
+        slidesPerView={3}
+        freeMode={true}
+        watchSlidesProgress={true}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="thumbs-swiper"
+      >
+        {images &&
+          images.map((image) => {
+            return (
+              <SwiperSlide key={image.url}>
+                <img id="thumbs-swiper_image" src={image.url} alt="product image thumb" />
+              </SwiperSlide>
+            );
+          })}
+      </SwiperReact>
+    </Paper>
+  );
+};
