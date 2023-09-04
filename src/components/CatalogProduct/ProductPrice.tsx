c28987a289da8b26c790f7a95a1a07f05af34f3f
OKitel <kitelolga@gmail.com>
2023-09-04 22:19:19 +0200

    feat: add discount price for product page and product card

--- src/components/CatalogProduct/ProductPrice.tsx
+++ src/components/CatalogProduct/ProductPrice.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Typography, Badge, Box } from '@mui/material';
 import { ProductVariant } from '@commercetools/platform-sdk';
 
 import { useAppSelector } from '../../store/hooks';
-import { findPriceWithCurrencyCode, formatPriceCents } from '../../utils/productsUtils';
+import {
+  findDiscountPriceWithCurrencyCode,
+  findPriceWithCurrencyCode,
+  formatPriceCents,
+} from '../../utils/productsUtils';
 import { TEXT_CONTENT } from '../consts';
 
 type ProductPriceProps = {
@@ -16,13 +20,53 @@ export const ProductPrice: React.FC<ProductPriceProps> = ({ selectedVariant }): 
 
   const { prices } = selectedVariant;
   const currencyPrice = findPriceWithCurrencyCode(prices, currency);
+  const currencyPriceWithDiscount = findDiscountPriceWithCurrencyCode(prices, currency);
   const priceValue = currencyPrice
     ? formatPriceCents(currencyPrice.value.centAmount, localization, currency)
     : TEXT_CONTENT.products.emptyPrice;
 
+  const discountPriceValue = currencyPriceWithDiscount
+    ? formatPriceCents(currencyPriceWithDiscount.value.centAmount, localization, currency)
+    : TEXT_CONTENT.products.emptyPrice;
+
   return (
-    <Typography align="center" fontSize={23} fontWeight="bold">
-      {priceValue}
-    </Typography>
+    <>
+      {!currencyPriceWithDiscount && (
+        <Typography align="center" fontSize={23} fontWeight="bold">
+          {priceValue}
+        </Typography>
+      )}
+      {currencyPriceWithDiscount && (
+        <>
+          <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+            <Typography
+              align="center"
+              fontSize={18}
+              fontWeight="bold"
+              sx={{ color: 'gray', textDecoration: 'line-through', marginTop: 0, lineHeight: 1 }}
+            >
+              {priceValue}
+            </Typography>
+            <Badge
+              badgeContent="Sale -10%"
+              color="secondary"
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+            >
+              <Typography
+                align="center"
+                fontSize={23}
+                fontWeight="bold"
+                sx={{ pt: '0.3rem', marginTop: 0, padding: '0 2rem' }}
+              >
+                {discountPriceValue}
+              </Typography>
+            </Badge>
+          </Box>
+        </>
+      )}
+    </>
   );
 };
