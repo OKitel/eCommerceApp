a93394150a3ecb8f256388be8aa9218dd152665f
Artem Lobovskii <lobovskiy@yandex.ru>
2023-09-05 23:30:28 +0300

    fix: mock swiper lib components to work around with its es6 modules

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -3,6 +3,17 @@ import { MemoryRouter } from 'react-router-dom';
 import { renderWithProviders } from './test-utils';
 import { waitFor } from '@testing-library/react';
 
+// mock swiper modules to fix https://alobovskiy-1691010266635.atlassian.net/browse/ECA-150
+jest.mock('swiper/react', () => ({
+  Swiper: ({ children }: { children: React.ReactElement }): JSX.Element => <div>{children}</div>,
+}));
+jest.mock('swiper/modules', () => ({
+  default: jest.fn(),
+  Navigation: jest.fn(),
+  Thumbs: jest.fn(),
+  FreeMode: jest.fn(),
+}));
+
 test('Render app correctly', async () => {
   renderWithProviders(
     <MemoryRouter initialEntries={['/']}>
