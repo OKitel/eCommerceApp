d299146952c6c77a796ae7de990f70d6a2fddca2
Artem Lobovskii <lobovskiy@yandex.ru>
2023-09-05 23:30:28 +0300

    fix: mock swiper lib components to work around with its es6 modules

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,12 @@ const config: Config = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/src/__mocks__/fileMock.ts',
     '\\.(css|s[ca]ss|less)$': '<rootDir>/src/__mocks__/styleMock.ts',
+    'swiper/css': '<rootDir>/src/__mocks__/styleMock.ts',
+    'swiper/css/bundle': '<rootDir>/src/__mocks__/styleMock.ts',
+    'swiper/css/autoplay': '<rootDir>/src/__mocks__/styleMock.ts',
+    'swiper/css/free-mode': '<rootDir>/src/__mocks__/styleMock.ts',
+    'swiper/css/navigation': '<rootDir>/src/__mocks__/styleMock.ts',
+    'swiper/css/pagination': '<rootDir>/src/__mocks__/styleMock.ts',
   },
   roots: ['<rootDir>/src'],
   testEnvironment: 'jsdom',
--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -3,6 +3,17 @@ import { MemoryRouter } from 'react-router-dom';
 import { renderWithProviders } from './test-utils';
 import { waitFor } from '@testing-library/react';
 
+jest.mock('swiper/react', () => ({
+  Swiper: ({ children }: { children: React.ReactElement }): JSX.Element => <div>{children}</div>,
+}));
+
+jest.mock('swiper/modules', () => ({
+  default: jest.fn(),
+  Navigation: jest.fn(),
+  Thumbs: jest.fn(),
+  FreeMode: jest.fn(),
+}));
+
 test('Render app correctly', async () => {
   renderWithProviders(
     <MemoryRouter initialEntries={['/']}>
