8bef833bbf076a140bd9cdadac4be452f534e945
Olga Kitel <79774026+OKitel@users.noreply.github.com>
2023-09-10 18:38:56 +0200

    refactor: update mockProductType
    Co-authored-by: Artem <88252084+lobovskiy@users.noreply.github.com>

--- src/__tests__/ProductFilterMain.test.tsx
+++ src/__tests__/ProductFilterMain.test.tsx
@@ -8,47 +8,644 @@ import { store } from '../store/store';
 import { ProductFilterMain } from '../components/ProductList/ProductFilterMain/ProductFilterMain';
 
 const mockProductType: ProductType = {
-  id: '862048f7-f470-4ec3-b77e-f8c7008ce96c',
-  name: 'Behringer UMX610 USB/MIDI Keyboard',
-  description:
-    'USB/MIDI keyboard features 61 high-quality full-size keys, real-time control and playability<br>\nUSB/Audio interface for recording and playback of any digital music files<br>\nWorks with PC or Mac computers and operating systems Mac OS X and Windows XP, Vista<br>\nIntuitive NI KorePlayer sound module with a 300MB ready-to-use sound library, including almost all categories/kinds of sounds, from synths, acoustic instruments, to drums<br>\n8 knobs and 10 assignable switches<br>\nFull range of 128 tones via octave shift function with multipurpose LED indicator<br>\nA separate midi output allows you to control external samplers, synthesizers and other equipment<br>\nRuns on batteries or power supply, as well as via USB',
-  createdAt: '2023-08-29T00:04:13.724Z',
-  lastModifiedAt: '2023-08-29T00:04:13.724Z',
-  version: 1,
+  id: '9e1fc8c0-e73d-426f-b008-c9e49798036a',
+  version: 2,
+  createdAt: '2023-08-29T00:04:12.373Z',
+  lastModifiedAt: '2023-09-02T13:30:43.837Z',
+  name: 'main',
+  description: 'Sunrise Product Data Set Structure',
   attributes: [
     {
-      name: 'attribute1',
+      name: 'creationDate',
       label: {
-        en: 'Attribute 1',
+        en: 'Date of Creation',
       },
-      type: { name: 'boolean' },
-      isRequired: true,
+      isRequired: false,
+      type: {
+        name: 'datetime',
+      },
       attributeConstraint: 'None',
-      inputHint: 'SingleLine',
       isSearchable: true,
+      inputHint: 'SingleLine',
     },
     {
-      name: 'attribute2',
+      name: 'brand',
       label: {
-        en: 'Attribute 2',
+        en: 'Brand',
       },
+      isRequired: true,
       type: {
         name: 'lenum',
         values: [
           {
-            key: 'value3',
+            key: 'artesia',
             label: {
-              en: 'Value 3',
+              en: 'Artesia',
+            },
+          },
+          {
+            key: 'ringway',
+            label: {
+              en: 'Ringway',
+            },
+          },
+          {
+            key: 'yamaha',
+            label: {
+              en: 'Yamaha',
+            },
+          },
+          {
+            key: 'samMartin',
+            label: {
+              en: 'Sam Martin',
+            },
+          },
+          {
+            key: 'behringer',
+            label: {
+              en: 'Behringer',
+            },
+          },
+          {
+            key: 'cme',
+            label: {
+              en: 'CME',
+            },
+          },
+          {
+            key: 'aria',
+            label: {
+              en: 'Aria',
+            },
+          },
+          {
+            key: 'greenland',
+            label: {
+              en: 'Greenland',
+            },
+          },
+          {
+            key: 'sevillia',
+            label: {
+              en: 'Sevillia',
+            },
+          },
+          {
+            key: 'schecter',
+            label: {
+              en: 'Schecter',
+            },
+          },
+          {
+            key: 'kaimana',
+            label: {
+              en: 'Kaimana',
+            },
+          },
+          {
+            key: 'meinl',
+            label: {
+              en: 'Meinl',
+            },
+          },
+          {
+            key: 'proMark',
+            label: {
+              en: 'ProMark',
+            },
+          },
+          {
+            key: 'hitman',
+            label: {
+              en: 'Hitman',
+            },
+          },
+          {
+            key: 'virtuozo',
+            label: {
+              en: 'Virtuozo',
+            },
+          },
+          {
+            key: 'tomasVagner',
+            label: {
+              en: 'Tomas Vagner',
             },
           },
         ],
       },
-      isRequired: true,
-      attributeConstraint: 'None',
-      inputHint: 'SingleLine',
+      attributeConstraint: 'SameForAll',
       isSearchable: true,
+      inputHint: 'SingleLine',
+    },
+    {
+      name: 'manufacturer',
+      label: {
+        en: 'Manufacturer',
+      },
+      isRequired: false,
+      type: {
+        name: 'lenum',
+        values: [
+          {
+            key: 'china',
+            label: {
+              en: 'China',
+            },
+          },
+          {
+            key: 'indonesia',
+            label: {
+              en: 'Indonesia',
+            },
+          },
+          {
+            key: 'india',
+            label: {
+              en: 'India',
+            },
+          },
+          {
+            key: 'japan',
+            label: {
+              en: 'Japan',
+            },
+          },
+          {
+            key: 'southKorea',
+            label: {
+              en: 'South Korea',
+            },
+          },
+          {
+            key: 'usa',
+            label: {
+              en: 'USA',
+            },
+          },
+          {
+            key: 'thailand',
+            label: {
+              en: 'Thailand',
+            },
+          },
+        ],
+      },
+      attributeConstraint: 'SameForAll',
+      isSearchable: true,
+      inputHint: 'SingleLine',
+    },
+    {
+      name: 'color',
+      label: {
+        en: 'Color',
+      },
+      isRequired: false,
+      type: {
+        name: 'lenum',
+        values: [
+          {
+            key: 'blue',
+            label: {
+              en: 'Blue',
+            },
+          },
+          {
+            key: 'pink',
+            label: {
+              en: 'Pink',
+            },
+          },
+          {
+            key: 'white',
+            label: {
+              en: 'White',
+            },
+          },
+          {
+            key: 'black',
+            label: {
+              en: 'Black',
+            },
+          },
+          {
+            key: 'rosewood',
+            label: {
+              en: 'Rosewood',
+            },
+          },
+          {
+            key: 'blackGlossBlack',
+            label: {
+              en: 'Black gloss black',
+            },
+          },
+          {
+            key: 'whiteAsh',
+            label: {
+              en: 'White ash',
+            },
+          },
+          {
+            key: 'satinBlackWalnut',
+            label: {
+              en: 'Satin black walnut',
+            },
+          },
+          {
+            key: 'satinDarkWalnut',
+            label: {
+              en: 'Satin dark walnut',
+            },
+          },
+          {
+            key: 'satinMahogany',
+            label: {
+              en: 'Satin mahogany',
+            },
+          },
+          {
+            key: 'silver',
+            label: {
+              en: 'Silver',
+            },
+          },
+          {
+            key: 'natural',
+            label: {
+              en: 'Natural',
+            },
+          },
+          {
+            key: 'naturalSatin',
+            label: {
+              en: 'Natural satin',
+            },
+          },
+          {
+            key: 'naturalMatte',
+            label: {
+              en: 'Natural matte',
+            },
+          },
+          {
+            key: 'brownSunburst',
+            label: {
+              en: 'Brown sunburst',
+            },
+          },
+          {
+            key: 'tobaccoSunburst',
+            label: {
+              en: 'Tobacco sunburst',
+            },
+          },
+          {
+            key: 'blueSunburst',
+            label: {
+              en: 'Blue sunburst',
+            },
+          },
+          {
+            key: '3ToneSunburst',
+            label: {
+              en: '3 tone sunburst',
+            },
+          },
+          {
+            key: 'stainedBrown',
+            label: {
+              en: 'Stained brown',
+            },
+          },
+          {
+            key: 'metallicBlue',
+            label: {
+              en: 'Metallic blue',
+            },
+          },
+          {
+            key: 'candyAppleRed',
+            label: {
+              en: 'Candy apple red',
+            },
+          },
+          {
+            key: 'metallicSilver',
+            label: {
+              en: 'Metallic silver',
+            },
+          },
+          {
+            key: 'seeThroughBlue',
+            label: {
+              en: 'See through blue',
+            },
+          },
+          {
+            key: 'seeThroughWineRed',
+            label: {
+              en: 'See through wine red',
+            },
+          },
+          {
+            key: 'metallicBlack',
+            label: {
+              en: 'Metallic black',
+            },
+          },
+          {
+            key: 'metallicRed',
+            label: {
+              en: 'Metallic red',
+            },
+          },
+          {
+            key: 'midnightSatinBlack',
+            label: {
+              en: 'Midnight satin black',
+            },
+          },
+          {
+            key: 'hotRodRed',
+            label: {
+              en: 'Hot rod red',
+            },
+          },
+          {
+            key: 'blackCherryBurst',
+            label: {
+              en: 'Black cherry burst',
+            },
+          },
+          {
+            key: 'charcoalBurst',
+            label: {
+              en: 'Charcoal burst',
+            },
+          },
+          {
+            key: 'crimsonRedBurst',
+            label: {
+              en: 'Crimson red burst',
+            },
+          },
+          {
+            key: 'crimsonRed',
+            label: {
+              en: 'Crimson red',
+            },
+          },
+          {
+            key: 'satinWhite',
+            label: {
+              en: 'Satin white',
+            },
+          },
+          {
+            key: 'satinSilver',
+            label: {
+              en: 'Satin silver',
+            },
+          },
+          {
+            key: 'walnutSatin',
+            label: {
+              en: 'Walnut satin',
+            },
+          },
+          {
+            key: 'electricBlue',
+            label: {
+              en: 'Electric blue',
+            },
+          },
+          {
+            key: 'satinBlack',
+            label: {
+              en: 'Satin black',
+            },
+          },
+          {
+            key: 'satinMetallicLightBlue',
+            label: {
+              en: 'Satin metallic light blue',
+            },
+          },
+          {
+            key: 'oceanBlueBurst',
+            label: {
+              en: 'Ocean blue burst',
+            },
+          },
+          {
+            key: 'vintageSunburst',
+            label: {
+              en: 'Vintage sunburst',
+            },
+          },
+          {
+            key: 'electricMagenta',
+            label: {
+              en: 'Electric magenta',
+            },
+          },
+          {
+            key: 'titanium',
+            label: {
+              en: 'Titanium',
+            },
+          },
+          {
+            key: 'vintageWhite',
+            label: {
+              en: 'Vintage white',
+            },
+          },
+          {
+            key: 'blackCherry',
+            label: {
+              en: 'Black cherry',
+            },
+          },
+          {
+            key: 'agedBlackSatin',
+            label: {
+              en: 'Aged black satin',
+            },
+          },
+          {
+            key: 'charcoal',
+            label: {
+              en: 'Charcoal',
+            },
+          },
+          {
+            key: 'seeThruBlueBurst',
+            label: {
+              en: 'See thru blue burst',
+            },
+          },
+          {
+            key: 'auroraBurst',
+            label: {
+              en: 'Aurora burst',
+            },
+          },
+          {
+            key: 'aquaBurst',
+            label: {
+              en: 'Aqua burst',
+            },
+          },
+          {
+            key: 'transBlackBurst',
+            label: {
+              en: 'Trans black burst',
+            },
+          },
+          {
+            key: 'satinCharcoalBurst',
+            label: {
+              en: 'Satin charcoal burst',
+            },
+          },
+          {
+            key: 'satinSkyBurst',
+            label: {
+              en: 'Satin sky burst',
+            },
+          },
+          {
+            key: 'satinInfernoBurst',
+            label: {
+              en: 'Satin inferno burst',
+            },
+          },
+          {
+            key: 'blueMetallic',
+            label: {
+              en: 'Blue metallic',
+            },
+          },
+          {
+            key: 'redMetallic',
+            label: {
+              en: 'Red metallic',
+            },
+          },
+          {
+            key: 'yellowNaturalSatin',
+            label: {
+              en: 'Yellow natural satin',
+            },
+          },
+          {
+            key: 'seeThruBlack',
+            label: {
+              en: 'See thru black',
+            },
+          },
+          {
+            key: 'violinSunburst',
+            label: {
+              en: 'Violin sunburst',
+            },
+          },
+          {
+            key: 'cherrySunburst',
+            label: {
+              en: 'Cherry sunburst',
+            },
+          },
+          {
+            key: 'redSunburst',
+            label: {
+              en: 'Red sunburst',
+            },
+          },
+          {
+            key: 'pinkMatte',
+            label: {
+              en: 'Pink matte',
+            },
+          },
+          {
+            key: 'purpleMatte',
+            label: {
+              en: 'Purple matte',
+            },
+          },
+          {
+            key: 'redMatte',
+            label: {
+              en: 'Red matte',
+            },
+          },
+          {
+            key: 'blueMatte',
+            label: {
+              en: 'Blue matte',
+            },
+          },
+          {
+            key: 'greenMatte',
+            label: {
+              en: 'Green matte',
+            },
+          },
+          {
+            key: 'yellowMatte',
+            label: {
+              en: 'Yellow matte',
+            },
+          },
+          {
+            key: 'whiteMatte',
+            label: {
+              en: 'White matte',
+            },
+          },
+          {
+            key: 'naturalMeranti',
+            label: {
+              en: 'Natural meranti',
+            },
+          },
+          {
+            key: 'purple',
+            label: {
+              en: 'Purple',
+            },
+          },
+          {
+            key: 'red',
+            label: {
+              en: 'Red',
+            },
+          },
+        ],
+      },
+      attributeConstraint: 'None',
+      isSearchable: true,
+      inputHint: 'SingleLine',
+    },
+    {
+      name: 'isOnStock',
+      label: {
+        en: 'On stock',
+      },
+      isRequired: false,
+      type: {
+        name: 'boolean',
+      },
+      attributeConstraint: 'None',
+      isSearchable: true,
+      inputHint: 'SingleLine',
     },
   ],
+  key: 'main',
 };
 
 const mockProductTypeWithoutAttributes = { ...mockProductType, attributes: null };
