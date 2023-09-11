35950d56890295356ed99e5823f3197c7b0fbeb8
OKitel <kitelolga@gmail.com>
2023-09-11 22:22:09 +0200

    feat: add EmptyCart component

---
+++ src/components/EmptyCart/EmptyCart.tsx
@@ -0,0 +1,19 @@
+import { Box, Button, Typography } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+
+import { LINKS } from '../../components/consts';
+import emptyCart from '../../assets/images/empty_cart.png';
+
+import './styles.scss';
+
+export const EmptyCart = (): React.ReactElement => {
+  return (
+    <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column" sx={{ mt: 5 }}>
+      <Typography variant="h2">Your cart is empty</Typography>
+      <img className="empty-cart-img" src={emptyCart} alt="empty cart" />
+      <Button component={RouterLink} to={LINKS.catalog} color="primary" variant="contained">
+        Back to catalog
+      </Button>
+    </Box>
+  );
+};
