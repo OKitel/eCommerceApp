8ab777f1f7abc4aee38b08909f812141b4e81c1f
OKitel <kitelolga@gmail.com>
2023-09-14 01:16:30 +0200

    feat: add optional rendering for empty cart

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,23 +1,43 @@
 import { Box, Container, Paper } from '@mui/material';
 
+import { useAppSelector } from '../../store/hooks';
+
 import { CartStepper } from '../../components/CartStepper/CartStepper';
 import { CartLineItem } from '../../components/CartLineItem/CartLineItem';
 import { CartSummary } from '../../components/CartSummary/CartSummary';
+import { EmptyCart } from '../../components/EmptyCart/EmptyCart';
 
 import './styles.scss';
 
 export const Cart: React.FC = (): JSX.Element => {
+  const { activeCart } = useAppSelector((state) => state.cart);
+
+  if (activeCart) {
+    const { lineItems } = activeCart;
+
+    if (lineItems.length !== 0) {
+      return (
+        <>
+          <Container sx={{ mt: 5 }}>
+            <CartStepper />
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start', gap: '1rem' }}>
+              <Paper elevation={3} sx={{ flex: '60%', padding: '2rem' }}>
+                {lineItems.map((item) => {
+                  console.log(item);
+                  return <CartLineItem />;
+                })}
+              </Paper>
+              <CartSummary />
+            </Box>
+          </Container>
+        </>
+      );
+    }
+  }
+
   return (
     <>
-      <Container sx={{ mt: 5 }}>
-        <CartStepper />
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start', gap: '1rem' }}>
-          <Paper elevation={3} sx={{ flex: '60%', padding: '2rem' }}>
-            <CartLineItem />
-          </Paper>
-          <CartSummary />
-        </Box>
-      </Container>
+      <EmptyCart />
     </>
   );
 };
