dbff7f2afdcd4c8e158501d85471d2ec97ac8a17
OKitel <kitelolga@gmail.com>
2023-09-14 17:59:46 +0200

    refactor: extract inline styles into classes for CartStepper component

--- src/components/CartStepper/CartStepper.tsx
+++ src/components/CartStepper/CartStepper.tsx
@@ -3,13 +3,15 @@ import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 
+import './styles.scss';
+
 const steps = ['Your cart', 'Delivery and payment', 'Done'];
 
 export const CartStepper = (): React.ReactElement => {
   const activeStep = 0;
 
   return (
-    <Box sx={{ width: '100%', mb: 3 }}>
+    <Box className="cart-stepper_container">
       <Stepper activeStep={activeStep}>
         {steps.map((label) => {
           return (
