ab4a7e0a0e9fde6381ee6cc8c939021e7a35a247
Artem Lobovskii <lobovskiy@yandex.ru>
2023-09-15 01:26:10 +0300

    fix: correct props for product details test

--- src/__tests__/ProductDetails.test.tsx
+++ src/__tests__/ProductDetails.test.tsx
@@ -5,6 +5,7 @@ import { renderWithProviders } from './test-utils';
 import { ProductDetails } from '../components/ProductDetails/ProductDetails';
 
 describe('ProductDetails', () => {
+  const productId = 'string';
   const productName = 'Test Product';
   const product = {
     name: { en: productName },
@@ -18,17 +19,17 @@ describe('ProductDetails', () => {
     searchKeywords: {},
   };
   it('renders product name', () => {
-    renderWithProviders(<ProductDetails product={product} />);
+    renderWithProviders(<ProductDetails productData={product} productId={productId} />);
     expect(screen.getByText(productName)).toBeInTheDocument();
   });
 
   it('renders "Add to cart" button', () => {
-    renderWithProviders(<ProductDetails product={product} />);
+    renderWithProviders(<ProductDetails productData={product} productId={productId} />);
     expect(screen.getByText('Add to cart')).toBeInTheDocument();
   });
 
   it('renders manufacturer information if available', () => {
-    renderWithProviders(<ProductDetails product={product} />);
+    renderWithProviders(<ProductDetails productData={product} productId={productId} />);
 
     expect(screen.getByText('Made in China')).toBeInTheDocument();
   });
