d23563e43185c615d5b1619790a0156d35e489ab
Artem Lobovskii <lobovskiy@yandex.ru>
2023-09-15 03:14:18 +0300

    feat: add test for remove from cart button

--- src/__tests__/ProductDetails.test.tsx
+++ src/__tests__/ProductDetails.test.tsx
@@ -1,6 +1,11 @@
-import { screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { render, screen } from '@testing-library/react';
+import configureMockStore from 'redux-mock-store';
 
+import { store } from '../store/store';
 import { renderWithProviders } from './test-utils';
+import { mockCart } from '../__mocks__/cart';
+import { mockProduct } from '../__mocks__/products';
 
 import { ProductDetails } from '../components/ProductDetails/ProductDetails';
 
@@ -32,5 +37,28 @@ describe('ProductDetails', () => {
     renderWithProviders(<ProductDetails productData={product} productId={productId} />);
 
     expect(screen.getByText('Made in China')).toBeInTheDocument();
+  });
+
+  it('displays "Remove from cart" button if the product is in the cart', () => {
+    const mockStore = configureMockStore();
+    const state = {
+      ...store.getState(),
+      cart: {
+        activeCart: mockCart,
+        progress: { removingLineItem: false },
+      },
+      product: {
+        product: mockProduct,
+      },
+    };
+    const mockedStore = mockStore(() => state);
+
+    render(
+      <Provider store={mockedStore}>
+        <ProductDetails productData={mockProduct.masterData.current} productId={mockProduct.id} />
+      </Provider>,
+    );
+
+    expect(screen.getByText('Remove from cart')).toBeInTheDocument();
   });
 });
