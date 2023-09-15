ab17ccc586d0221eb8b8656fb3d90c23e4ff1de1
OKitel <kitelolga@gmail.com>
2023-09-15 02:17:06 +0200

    feat: add user friendly message with link to catalog

--- src/components/EmptyCart/EmptyCart.tsx
+++ src/components/EmptyCart/EmptyCart.tsx
@@ -13,8 +13,15 @@ export const EmptyCart = (): React.ReactElement => {
         Your cart is empty
       </Typography>
       <img className="empty-cart-img" src={emptyCart} alt="empty cart" />
+      <Typography variant="body1" className="empty-cart_text">
+        Time to &nbsp;
+        <RouterLink to={LINKS.catalog} className="empty-cart_link">
+          add some items
+        </RouterLink>
+        &nbsp; to your cart and make it smile!
+      </Typography>
       <Button component={RouterLink} to={LINKS.catalog} color="primary" variant="contained">
-        Back to catalog
+        Start Shopping
       </Button>
     </Box>
   );
