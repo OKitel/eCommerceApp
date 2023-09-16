b718b43d03fe3cf1eb2f86ff3cf123df2d1840d2
Artem Lobovskii <lobovskiy@yandex.ru>
2023-09-17 02:35:22 +0300

    feat: change cart currency on change currency selector

--- src/components/Header/CurrencySelector.tsx
+++ src/components/Header/CurrencySelector.tsx
@@ -4,11 +4,16 @@ import { FormControl, MenuItem, Select } from '@mui/material';
 import { Currencies } from '../../types';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import { changeSettings } from '../../slices/settings/slice';
+import { changeCartCurrency } from '../../slices/cart/slice';
 import { isCurrency } from '../../utils/typesUtils';
 
 export const CurrencySelector: React.FC = (): JSX.Element => {
   const currencies = Object.values(Currencies);
   const currencySelected = useAppSelector((state) => state.settings.currency);
+  const {
+    activeCart,
+    progress: { changeCartCurrency: progressChangeCartCurrency },
+  } = useAppSelector((state) => state.cart);
   const dispatch = useAppDispatch();
 
   return (
@@ -19,10 +24,15 @@ export const CurrencySelector: React.FC = (): JSX.Element => {
         id="currency-select"
         value={currencySelected}
         size="small"
+        disabled={progressChangeCartCurrency}
         inputProps={{ 'aria-label': 'Without label' }}
         onChange={({ target: { value } }): void => {
           if (isCurrency(value)) {
             dispatch(changeSettings({ currency: value }));
+
+            if (activeCart) {
+              dispatch(changeCartCurrency(value));
+            }
           }
         }}
         data-testid="currency-select"
